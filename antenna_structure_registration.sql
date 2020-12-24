2332ec2faa071c21752765ccff524bbecf5bea88
Daniel Washburn <daniel@washburn.at>
2020-12-23 23:50:03 -0500

    Dimension tables for ASR schema

--- antenna_structure_registration.sql
+++ antenna_structure_registration.sql
@@ -1,5 +1,6 @@
 -- Antenna Structure Registration (ASR)
 CREATE SCHEMA IF NOT EXISTS asr;
+SET search_path TO asr;
 
 -- Attachments
 CREATE TABLE IF NOT EXISTS asr.at
@@ -13,6 +14,8 @@ CREATE TABLE IF NOT EXISTS asr.at
 	description               varchar(60)          null,
 	date                      char(10)             null
 );
+
+COMMENT ON TABLE asr.at IS 'Attachments';
 
 -- Tower Coordinates
 CREATE TABLE IF NOT EXISTS asr.co
@@ -36,6 +39,8 @@ CREATE TABLE IF NOT EXISTS asr.co
     array_tower_position      int                  null,
     array_total_tower         int                  null
 );
+
+COMMENT ON TABLE asr.co IS 'Tower Coordinates';
 
 -- Determinations
 CREATE TABLE IF NOT EXISTS asr.de
@@ -72,6 +77,7 @@ CREATE TABLE IF NOT EXISTS asr.de
 	version                   int                  null
 );
 
+COMMENT ON TABLE asr.de IS 'Determinations';
 
 -- Determination Painting and Lighting Data
 CREATE TABLE IF NOT EXISTS asr.dp
@@ -82,6 +88,7 @@ CREATE TABLE IF NOT EXISTS asr.dp
 	specification_option      int                  null
 );
 
+COMMENT ON TABLE asr.dp IS 'Determination Painting and Lighting Data';
 
 -- FAA Determination Remarks
 CREATE TABLE IF NOT EXISTS asr.dr
@@ -93,6 +100,7 @@ CREATE TABLE IF NOT EXISTS asr.dr
 	remark_text               varchar(255)         null
 );
 
+COMMENT ON TABLE asr.dr IS 'FAA Determination Remarks';
 
 -- Entity
 CREATE TABLE IF NOT EXISTS asr.en
@@ -124,6 +132,7 @@ CREATE TABLE IF NOT EXISTS asr.en
     frn                       char(10)             null       
 );
 
+COMMENT ON TABLE asr.en IS 'Entity';
 
 -- History
 CREATE TABLE IF NOT EXISTS asr.hs
@@ -137,6 +146,7 @@ CREATE TABLE IF NOT EXISTS asr.hs
 	description               varchar(50)          null
 );
 
+COMMENT ON TABLE asr.hs IS 'Entity';
 
 -- Registration Data
 CREATE TABLE IF NOT EXISTS asr.ra
@@ -192,6 +202,7 @@ CREATE TABLE IF NOT EXISTS asr.ra
 	date_signed_or            char(10)             null
 );
 
+COMMENT ON TABLE asr.ra IS 'Registration Data';
 
 -- Remarks
 CREATE TABLE IF NOT EXISTS asr.re
@@ -207,6 +218,7 @@ CREATE TABLE IF NOT EXISTS asr.re
 	remark_text               varchar(255)         null
 );
 
+COMMENT ON TABLE asr.re IS 'Remarks';
 
 -- Reason
 CREATE TABLE IF NOT EXISTS asr.rs
@@ -222,6 +234,7 @@ CREATE TABLE IF NOT EXISTS asr.rs
 	reason_date               char(10)             null
 );
 
+COMMENT ON TABLE asr.re IS 'Reason';
 
 -- Special Conditions
 CREATE TABLE IF NOT EXISTS asr.sc
@@ -236,6 +249,7 @@ CREATE TABLE IF NOT EXISTS asr.sc
 	remark_text               varchar(255)         null
 );
 
+COMMENT ON TABLE asr.sc IS 'Special Conditions';
 
 -- Environmental Compliance
 CREATE TABLE IF NOT EXISTS asr.ec
@@ -255,3 +269,208 @@ CREATE TABLE IF NOT EXISTS asr.ec
 	env_cert_basis            char(1)	       null,
 	local_notice_date         timestamp             null
 );
+
+COMMENT ON TABLE asr.ec IS 'Environmental Compliance';
+
+CREATE TABLE IF NOT EXISTS asr.content_indicator
+(
+	indicator 	char(3) primary key,
+	description varchar(12)
+);
+
+COMMENT ON TABLE asr.content_indicator IS 'Content Indicator';
+
+TRUNCATE TABLE asr.content_indicator;
+INSERT INTO asr.content_indicator (indicator, description) VALUES
+	('APP', 'Application'),
+	('REG', 'Registration')
+;
+
+CREATE TABLE IF NOT EXISTS asr.application_purpose
+(
+	purpose 	char(2)	PRIMARY KEY,
+	description	varchar(100)
+);
+
+COMMENT ON TABLE asr.application_purpose IS 'Application Purpose';
+
+TRUNCATE TABLE asr.application_purpose;
+INSERT INTO asr.application_purpose (purpose, description) VALUES
+	('AM', 'Amendment of a previously filed, currently pending application'),
+	('AU', 'Administrative Update'),
+	('CA', 'Cancellation of a registered, unconstructed structure'),
+	('DI', 'Notification of antenna structure dismantlement'),
+	('DU', 'Request a duplicate Registration document'),
+	('MD', 'Modification of a registered antenna structure'),
+	('NE', 'Application to register an antenna structure that has not been registered'),
+	('NT', 'Notification that an antenna structure has been constructed'),
+	('OC', 'Ownership change'),
+	('RE', 'Registration of a replacement antenna structure WD Withdrawal of a pending application')
+;
+
+CREATE TABLE IF NOT EXISTS asr.input_source_code
+(
+	code	char(1) PRIMARY KEY;
+	description	varchar(12)
+);
+
+COMMENT ON TABLE asr.input_source_code IS 'Input Source Code';
+
+TRUNCATE TABLE asr.input_source_code;
+INSERT INTO asr.input_source_code (code, description) VALUES
+	('D', 'Diskette'),
+	('I', 'Interactive'),
+	('M', 'Manual')
+;
+
+CREATE TABLE IF NOT EXISTS asr.status_code
+(
+	code	char(1) PRIMARY KEY,
+	description	varchar(20)
+);
+
+COMMENT ON TABLE asr.status_code IS 'Status Code';
+
+TRUNCATE TABLE asr.status_code;
+INSERT INTO asr.status_code (code, description) VALUES
+	('A', 'Canceled'),
+	('C', 'Constructed'),
+	('D', 'Dismissed'),
+	('G', 'Granted'),
+	('I', 'Dismantled'),
+	('N', 'Inactive'),
+	('O', 'Owner removed'),
+	('P', 'Pending'),
+	('R', 'Returned'),
+	('T', 'Terminated'),
+	('W', 'Withdrawn')
+;
+
+CREATE TABLE IF NOT EXISTS asr.archive_flag_code
+(
+	code	char(1)	PRIMARY KEY,
+	description	varchar(16)
+);
+
+COMMENT ON TABLE asr.archive_flag_code IS 'Archive Flag Code';
+
+TRUNCATE TABLE asr.archive_flag_code;
+INSERT INTO asr.archive_flag_code (code, description) VALUES
+	('C', 'Current version'),
+	('A', 'Archive')
+;
+
+CREATE TABLE IF NOT EXISTS asr.specification_option
+(
+	option	char(1) PRIMARY KEY,
+	description	varchar(16)
+);
+
+COMMENT ON TABLE asr.specification_option IS 'Specification Option';
+
+TRUNCATE TABLE asr.specification_option;
+INSERT INTO asr.specification_option (option, description) VALUES
+	('1', 'FAA Chapter 1'),
+	('2', 'FAA Chapter 2'),
+	('3', 'FAA Chapter 3'),
+	('4', 'FCC Paragraphs')
+;
+
+CREATE TABLE IF NOT EXISTS asr.signature_code
+(
+	code	char(1)	PRIMARY KEY,
+	description	varchar(8)
+);
+
+COMMENT ON TABLE asr.signature_code IS 'Invalid signature code';
+
+TRUNCATE TABLE asr.signature_code;
+INSERT INTO asr.signature_code (code, description) VALUES
+	('N', 'Invalid'),
+	('Y', 'Valid')
+;
+
+CREATE TABLE IF NOT EXISTS asr.contact_type
+(
+	type	char(1)	PRIMARY KEY,
+	description	varchar(16)
+);
+
+COMMENT ON TABLE asr.contact_type IS 'Contact Type';
+
+TRUNCATE TABLE asr.contact_type;
+INSERT INTO asr.contact_type (type, description) VALUES
+	('O', 'Owner'),
+	('R', 'Representative'),
+	('A', 'Assignor')
+;
+
+CREATE TABLE IF NOT EXISTS asr.entity_type
+(
+	type	char(1)	PRIMARY KEY,
+	description	varchar(30)
+)
+
+COMMENT ON TABLE asr.entity_type IS 'Entity Type';
+
+TRUNCATE TABLE asr.entity_type;
+INSERT INTO asr.entity_type (type, description) VALUES
+	('O', 'Consortium'),
+	('C', 'Corporation'),
+	('D', 'General Partnership'),
+	('G', 'Government Entity'),
+	('I', 'Individual'),
+	('L', 'Limited Liability Company'),
+	('F', 'Limited Liability Partnership'),
+	('E', 'Limited Partnership'),
+	('H', 'Other'),
+	('T', 'Trust'),
+	('U', 'Unincorporated Association')
+;
+
+CREATE TABLE IF NOT EXISTS asr.coordinate_type
+(
+	type	char(1)	PRIMARY KEY,
+	description	varchar(5)
+);
+
+COMMENT ON TABLE asr.coordinate_type IS 'Coordinate Type';
+
+TRUNCATE TABLE asr.coordinate_type;
+INSERT INTO asr.coordinate_type (type, description) VALUES
+	('T', 'Tower'),
+	('A', 'Array')
+;
+
+CREATE TABLE IF NOT EXISTS asr.remark_type
+(
+	type	char(3)	PRIMARY KEY,
+	description	varchar(30)
+);
+
+COMMENT ON TABLE asr.remark_type IS 'Remark Type';
+
+TRUNCATE TABLE asr.remark_type;
+INSERT INTO asr.remark_type (type, description) VALUES
+	('FAA', 'FAA remark'),
+	('FCC', 'FCC (external) remark')
+;
+
+CREATE TABLE IF NOT EXISTS asr.attachment_code
+(
+	code	char(1)	PRIMARY KEY,
+	description	varchar(50)
+);
+
+COMMENT ON TABLE asr.attachment_code IS 'Attachment Code';
+
+TRUNCATE TABLE asr.attachment_code;
+INSERT INTO asr.attachment_code (code, description) VALUES
+	('C', 'Confidential'),
+	('E', 'Environmental Assessment (EA)'),
+	('F', 'Finding Of No Significant Impact (FONSI)'),
+	('I', 'Environmental Impact Statement (EIS)'),
+	('N', 'NEPA'),
+	('O', 'Other'),
+	('R', 'Record Of Decision W Waiver')
+;
